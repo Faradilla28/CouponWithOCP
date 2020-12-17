77d1b14b86aa2cb56ae5b2461df3a8edabe1423d
Faradilla28 <faradilla.2838@students.amikom.ac.id>
2020-12-17 11:30:32 +0700

    Add project files.

---
+++ CouponWithOCP/CouponWithCashBack.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CouponWithOCP
+{
+    class CouponWithCashBack : Coupon
+    {
+        public double discNominal;
+
+        public CouponWithCashBack(double discNominal)
+        {
+            this.discNominal = discNominal;
+        }
+
+        public override double calculate(double originPrice)
+        {
+            return originPrice - discNominal;
+        }
+    }
+}
